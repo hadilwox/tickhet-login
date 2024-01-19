1403fa5ecd2138bdf572bbb8a8ca5c940b386eb0
Hadilwox <hadilwox@gmail.com>
2024-01-19 11:54:40 +0330

    first commmit

---
+++ src/JS/login.js
@@ -0,0 +1,13 @@
+let $ = document;
+let inputs = $.querySelectorAll("input");
+let labels = $.querySelectorAll("label");
+
+// console.log(inputs);
+// inputs.forEach((input) => {
+//   labels.forEach((label) => {
+//     // console.log(Boolean(label));
+//     if (input === "hello") {
+//       label.classList.add("active-label");
+//     }
+//   });
+// });
