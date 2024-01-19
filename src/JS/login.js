4e4a1cd361fbcccc414563e3e00a8b6c641291da
Hadilwox <hadilwox@gmail.com>
2024-01-19 19:20:15 +0330

    add a see password

--- src/JS/login.js
+++ src/JS/login.js
@@ -1,6 +1,25 @@
 const $ = document;
 let regularEye = $.querySelector(".fa-regular");
 let solidEye = $.querySelector(".fa-solid");
+let inputPass = $.getElementById("pass");
+let inputEmail = $.getElementById("email");
 
+function clickEyePass() {
+  regularEye.addEventListener("click", () => {
+    if (inputPass.type == "password") {
+      inputPass.type = "text";
+      console.log("click");
+    } else {
+      inputPass.type = "password";
+    }
+  });
+}
 
+function testCorrectEmail() {
+  let regexEmail =
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    let EmailValue = inputEmail.value;
+    
+}
 
+clickEyePass();
