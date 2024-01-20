7f7bad62d610d45209de2645c39ad0ca893da20b
hadilwox <hadilwox@gmail.com>
2024-01-20 14:42:13 +0330

    add creat account in page

--- src/JS/login.js
+++ src/JS/login.js
@@ -1,8 +1,14 @@
 const $ = document;
+
 let regularEye = $.querySelector(".fa-regular");
 let solidEye = $.querySelector(".fa-solid");
 let inputPass = $.getElementById("pass");
 let inputEmail = $.getElementById("email");
+
+let loginSection = $.querySelector(".login-section")
+let signSection = $.querySelector(".sign-section")
+let creatAccountLink = $.querySelector(".creat-account-link")
+let loginNowLink = $.querySelector(".login-now-link")
 
 function clickEyePass() {
   regularEye.addEventListener("click", () => {
@@ -15,6 +21,23 @@ function clickEyePass() {
   });
 }
 
+function changeToSign (){
+  creatAccountLink.addEventListener("click", () =>{
+    loginSection.classList.add("activeRotate")
+    signSection.classList.add("activeRotate")
+    
+  })
+
+}
+function changeToLogin (){
+  loginNowLink.addEventListener("click", () =>{
+    loginSection.classList.remove("activeRotate")
+    signSection.classList.remove("activeRotate")
+    
+  })
+
+}
+
 function testCorrectEmail() {
   let regexEmail =
     /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
@@ -22,4 +45,7 @@ function testCorrectEmail() {
     
 }
 
+
+changeToLogin ()
 clickEyePass();
+changeToSign ()
