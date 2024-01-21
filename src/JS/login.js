707bd6301f1f9b22df8c46ded6db7db0ba2c2750
hadilwox <hadilwox@gmail.com>
2024-01-21 13:39:30 +0330

    changed warning message

--- src/JS/login.js
+++ src/JS/login.js
@@ -21,6 +21,9 @@ let loginNowLink = $.querySelector(".login-now-link")
 // Button to Enter
 let buttons = $.querySelectorAll(".btn")
 
+// Warning Message
+let warningMessage = $.querySelectorAll(".warning-message")
+
 // change type password (can see / can't see)
 function clickEyePass() {
   regularEye.forEach(eyeBtn => {
@@ -64,6 +67,7 @@ function changeToLogin (){
 
 }
 
+// RegEx Email to correrct
 function testCorrectEmail() {
   let regexEmail =
     /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
@@ -72,12 +76,14 @@ function testCorrectEmail() {
   if (isEmailLogin || isEmailSign){
     console.log(true);
     emptyInput();
+    showWelcamMesage();
   } else {
     showWarningMessage();
   }
     
 }
 
+// Event click on Btn
 function clickOnBtn (){
   buttons.forEach(btn => {
     btn.addEventListener("click", () => {
@@ -86,6 +92,7 @@ function clickOnBtn (){
   })
 }
 
+// Empty all inputs
 function emptyInput (){
 
   inputPassLogin.value = "";
@@ -95,9 +102,26 @@ function emptyInput (){
   inputUserName.value = "";
 }
 
+// show Not correct Email message
 function showWarningMessage (){
-  $.querySelectorAll(".warning-message").forEach( message => {
-    message.innerHTML = "ایمیل شما نامعتبر است"
+  warningMessage.forEach( message => {
+    message.className = "warning-message text-danger"
+    message.innerHTML = "ایمیل شما نامعتبر است";
+  })
+  setInterval(() => {
+    warningMessage.forEach( message => {
+      message.innerHTML = "";
+    })
+
+  }, 3000)
+
+}
+
+// show correct Enter
+function showWelcamMesage (){
+  warningMessage.forEach( message => {
+    message.className = "warning-message text-success "
+    message.innerHTML = "خوشامدید";
   })
 }
 
