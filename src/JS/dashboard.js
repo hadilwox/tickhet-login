16effc13b60044ed1eb6493497a208a9a5c5dbf2
Hadilwox <hadilwox@gmail.com>
2024-01-27 23:41:24 +0330

    make ticket condition

--- src/JS/dashboard.js
+++ src/JS/dashboard.js
@@ -1,10 +1,12 @@
 const $ = document;
 const sidebar = $.querySelector(".sidebar");
 const toggleBtnMenu = $.querySelector(".toggle-menu");
+const content = $.querySelector(".content");
 
 function clickMenuActive() {
   toggleBtnMenu.addEventListener("click", () => {
     sidebar.classList.toggle("active");
+    content.classList.toggle("active");
   });
 }
 
